496bc54b67abf8b4347ba19b109841f3ea36bc88
Jon Ng <jonathanng15@gmail.com>
2021-05-07 22:26:04 -0700

    Commented out unused code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 'use strict'
 
+
 document.addEventListener('DOMContentLoaded', () => {
   const mainForm = document.querySelector('#main-form');
   const form = document.querySelector('form');
-  const submitButton = form.querySelector('button');
+  // const submitButton = form.querySelector('button');
   const ul = document.querySelector('.main ul');
   const input = form.querySelector('input');
   const clearButton = mainForm.querySelector('#clear-button');
